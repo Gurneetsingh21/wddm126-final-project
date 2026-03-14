fc751b372e67cd85a8fe0ab7cac8a4a75882fa80
Gurneet Singh Saini <gurneetsaini53@gmail.com>
2026-03-14 16:49:19 -0400

    Add HTML structure, CSS styling, and JavaScript functionality

---
+++ src/script.js
@@ -0,0 +1,11 @@
+// Variable
+let projectName = "WDDM126 Final Project";
+
+// Function
+function greet() {
+    console.log("Welcome to my " + projectName + "!");
+}
+
+// Call the function
+greet();
+
